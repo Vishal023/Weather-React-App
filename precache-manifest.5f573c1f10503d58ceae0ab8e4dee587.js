cb36ee5b72a3861005b7e86b95e0be19539c74e6
Vishal Singh <v.singh.kld@gmail.com>
2020-10-19 16:31:51 +0530

    Updates

---
+++ precache-manifest.5f573c1f10503d58ceae0ab8e4dee587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2412fba48f975a87a6d7ac39ba806c43",
+    "url": "/Weather-React-App/index.html"
+  },
+  {
+    "revision": "dbeff812f988b41dc025",
+    "url": "/Weather-React-App/static/css/main.2ae6afdc.chunk.css"
+  },
+  {
+    "revision": "7ce66d4fc2713de697d6",
+    "url": "/Weather-React-App/static/js/2.cc28f713.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Weather-React-App/static/js/2.cc28f713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbeff812f988b41dc025",
+    "url": "/Weather-React-App/static/js/main.b7f387a4.chunk.js"
+  },
+  {
+    "revision": "baf16f5a4a144f8d5f7a",
+    "url": "/Weather-React-App/static/js/runtime-main.4c396e91.js"
+  }
+]);
